7ad217b0aebb40584a8d06d02b095dc47819971d
z-gock <942620282@qq.com>
2023-02-08 17:19:49 +1100

    first commit

---
+++ day_4/day4.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+from datetime import datetime
+print('hello')
+titanic=pd.read_csv("train.csv")
