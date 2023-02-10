4c911dec95ccffc10e2af2cd6b726083225021f7
z-gock <942620282@qq.com>
2023-02-11 01:23:14 +1100

    to day 30

---
+++ day_30.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+st.title('?? yt-img-app')
+st.header('YouTube Thumbnail Image Extractor App')
+
+with st.expander('About this app'):
+  st.write('This app retrieves the thumbnail image from a YouTube video.')
+
+# Image settings
+st.sidebar.header('Settings')
+img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
+selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
+img_quality = img_dict[selected_img_quality]
+
+yt_url = st.text_input('Paste YouTube URL', 'https://www.youtube.com/watch?v=QTWm_5xM9yE')
+
+def get_ytid(input_url):
+  if 'youtu.be' in input_url:
+    ytid = input_url.split('/')[-1]
+  if 'youtube.com' in input_url:
+    ytid = input_url.split('=')[-1]
+  return ytid
+
+# Display YouTube thumbnail image
+if yt_url != '':
+  ytid = get_ytid(yt_url) # yt or yt_url
+
+  yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
+  response = requests.get(yt_img)
+  img=response.content
+  st.image(yt_img)
+  st.write('YouTube video thumbnail image URL: ', yt_img)
+  st.download_button("download this pic",img,'yt_cover.jpg',"image/jpeg")
+else:
+  st.write('?? Enter URL to continue ...')
